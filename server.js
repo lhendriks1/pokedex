ffae8e101cae9969425cfd9563baab9d77eba877
Lydia Hendriks <hendriks.lydia@gmail.com>
2019-10-22 14:37:58 -0400

    prep for production

--- server.js
+++ server.js
@@ -1,15 +1,71 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+const helmet = require('helmet');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 const app = express();
-app.use(morgan('dev'));
+const PORT = process.env.PORT || 8000;
 
-app.use((req, res) => {
-    res.send("Hello, world!")
+
+//MIDDLEWARE
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(400).json({error: 'Unauthorized request'})
+    };
+
+    next()
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production'){
+        response = { error: {message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
 })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+//GET request handlers
+app.get('/types', function handleGetTypes(req, res) {
+    res.json(validTypes);
+});
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
+
+    if (req.query.name) {
+        response = response.filter(pokemon =>
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        )
+    }
+    if (req.query.type) {
+        function formatType(string) {
+            const lowercase = req.query.type.toLowerCase();
+            return lowercase.charAt(0).toUpperCase() + lowercase.substring(1)
+        };
+
+        if(!validTypes.includes(formatType(req.query.type))) {
+            res.status(400).json({error: "Invalid request"})
+        };
+
+        response = response.filter(pokemon =>
+            pokemon.type.includes(formatType(req.query.type))
+        );
+    }
+    res.json(response)
+});
+
+
+//Listen on PORT
+app.listen(PORT);
